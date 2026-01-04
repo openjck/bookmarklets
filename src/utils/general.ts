f8191ffd758828aeffdc407ca876ac540d42bce9
John Karahalis <john@johnkarahalis.com>
2026-01-04 13:59:30 -0500

    Add JSDoc comments to utils/general.ts

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,3 +1,13 @@
+/**
+ * Run a function and `alert` an error message if any error occurs.
+ *
+ * If any error is thrown while running the provided function, the `toString()`
+ * method of the error is called and an `alert` is raised with that text.
+ *
+ * If something that is thrown that is not an `Error` object, the item is
+ * printed to the console and an `alert` is raised stating that an unknown error
+ * occurred and that more information was printed to the console.
+ */
 export function alertOnError(fn: () => void): void {
   try {
     fn();
@@ -5,18 +15,31 @@ export function alertOnError(fn: () => void): void {
     if (err instanceof Error) {
       alert(err.toString());
     } else {
+      console.error("Unknown error:", err);
       alert(
         "An error of an unknown type occurred. " +
-          "It has been printed to the console.",
+          "More information has been printed to the console.",
       );
-      console.error(err);
     }
   }
 }
 
-// Important note: If the substring is not found, return the original string.
-export function removeFromEnd(str: string, toRemove: string): string {
-  const lastIndex = str.lastIndexOf(toRemove);
+/**
+ * Remove a substring from the end of another string, or return the original.
+ *
+ * If the provided substring does not appear at the end of the source string,
+ * return the source string unaltered.
+ *
+ * @param sourceString - The string that should have a substring removed from
+ *                       the end of it.
+ * @param substring - The substring to remove the end of the source string.
+ *
+ * @returns The source string with the provided substring removed, if it occurs
+ *          at the end of the source string, otherwise the source string
+ *          unaltered.
+ */
+export function removeFromEnd(str: string, removal: string): string {
+  const lastIndex = str.lastIndexOf(removal);
 
   if (lastIndex === -1) {
     return str;
